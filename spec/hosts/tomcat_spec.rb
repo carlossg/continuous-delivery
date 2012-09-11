0a9505ca5b630dd03180bd47eacd6722f5ea38e4
Carlos Sanchez <csanchez@maestrodev.com>
2012-09-11 15:13:26 +0200

    Add more specs

--- spec/hosts/tomcat_spec.rb
+++ spec/hosts/tomcat_spec.rb
@@ -4,4 +4,9 @@ describe 'tomcat1.acme.com' do
   let(:facts) { {:osfamily => 'RedHat', :operatingsystem => 'CentOS', :lsbmajdistrelease => 6} }
 
   it { should contain_class('java').with_distribution /openjdk/ }
+
+  it "configure webapp" do
+    should contain_maven('/srv/tomcat/appfuse/webapps/ROOT.war')
+    should contain_maven('/srv/tomcat/appfuse/webapps/ROOT/WEB-INF/lib/postgresql-9.1-901.jdbc4.jar')
+  end
 end
