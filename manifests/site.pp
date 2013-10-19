adbff604c2f556bb1b3e3732821568fa522060c4
Carlos Sanchez <csanchez@maestrodev.com>
2013-10-19 17:34:28 +0200

    Upgrade firewall module

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,21 +10,4 @@ node 'parent' {
     firewall => true
   }
 
-  # Always persist firewall rules
-  exec { 'persist-firewall':
-    command     => $operatingsystem ? {
-      'debian'          => '/sbin/iptables-save > /etc/iptables/rules.v4',
-      /(RedHat|CentOS)/ => '/sbin/iptables-save > /etc/sysconfig/iptables',
-    },
-    refreshonly => true,
-  }
-
-  # These defaults ensure that the persistence command is executed after
-  # every change to the firewall.
-  Firewall {
-    notify  => Exec['persist-firewall'],
-  }
-  Firewallchain {
-    notify  => Exec['persist-firewall'],
-  }
 }
