02ee83ba301f63a7e6e0fd3fb61cf9ad6f8d00e1
Carlos Sanchez <csanchez@maestrodev.com>
2014-01-12 18:48:26 +0100

    Add tests for firewall config

--- spec/hosts/www_spec.rb
+++ spec/hosts/www_spec.rb
@@ -5,4 +5,6 @@ describe 'www.acme.com' do
 
   it { should_not contain_class('java') }
   it { should contain_package('httpd') }
+  it { should contain_firewall('100 allow apache').with_action('accept') }
+  it { should have_firewall_resource_count(2) } # avahi and apache
 end
